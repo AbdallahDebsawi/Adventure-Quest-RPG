6411ad4cfd3890c56ddf4d6527fa78f3b1a1304c
AbdallahDebsawi <96738825+AbdallahDebsawi@users.noreply.github.com>
2024-07-08 23:37:34 +0300

    Adventure Quest RPG - Part 3

---
+++ AdventureQuestRPG/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureQuestRPG
+{
+    public class Inventory
+    {
+        private List<Item> items;
+
+        public Inventory()
+        {
+            items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+            Console.WriteLine($"You have obtained a {item.Name}!");
+        }
+
+        public void DisplayInventory()
+        {
+            Console.WriteLine("Inventory:");
+            if (items.Count == 0)
+            {
+                Console.WriteLine("Your inventory is empty.");
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    Console.WriteLine($"- {item.Name}: {item.Description}");
+                }
+            }
+        }
+
+        public Item GetItem(string itemName)
+        {
+            return items.Find(item => item.Name.Equals(itemName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void RemoveItem(Item item)
+        {
+            items.Remove(item);
+        }
+    }
+}
---
+++ AdventureQuestRPG/Items.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureQuestRPG
+{
+    public abstract class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        protected Item(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+
+    public class Weapon : Item
+    {
+        public int AttackPower { get; set; }
+
+        public Weapon(string name, string description, int attackPower)
+            : base(name, description)
+        {
+            AttackPower = attackPower;
+        }
+    }
+
+    public class Armor : Item
+    {
+        public int Defense { get; set; }
+
+        public Armor(string name, string description, int defense)
+            : base(name, description)
+        {
+            Defense = defense;
+        }
+    }
+
+    public class Potion : Item
+    {
+        public int HealthRestore { get; set; }
+
+        public Potion(string name, string description, int healthRestore)
+            : base(name, description)
+        {
+            HealthRestore = healthRestore;
+        }
+    }
+}
+
